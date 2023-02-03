d1887f48c47639b76fcb8d86eb302f8fe65c985d
zhouxzw <44952147+zhouxzw@users.noreply.github.com>
2023-02-02 19:39:47 -0700

    Update Intro.js

--- components/Intro.js
+++ components/Intro.js
@@ -5,11 +5,11 @@ function Intro(props) {
     <div className={styles.introContainer}>
       <article className={styles.intro}>
         My name is William, computer science graduate from the University of
-        Calgary. It wasn’t until my fourth year that I truly found a passion in
-        web development and decided to pursue a career in this field. Since
-        graduating I have helped local businesses develop static to full stack
-        websites. I hope to expand my knowledge within this field and be able to
-        solve complex problems through programming.
+        Calgary. It wasn’t until my later years during university that I truly
+        found a passion in web development and decided to pursue a career in
+        this field. Since graduating I have helped local businesses develop
+        static to full stack websites. I hope to expand my knowledge within this
+        field and be able to solve complex problems through programming.
         <p>I am currently open to any web development opportunities!</p>
       </article>
     </div>
