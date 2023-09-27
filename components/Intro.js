1bc2910df183b07783210fa3f79d1ba485389a59
zhouxzw <44952147+zhouxzw@users.noreply.github.com>
2023-09-26 20:28:03 -0600

    Update Intro.js

--- components/Intro.js
+++ components/Intro.js
@@ -6,7 +6,7 @@ function Intro(props) {
       <article className={styles.intro}>
         My name is William, computer science graduate from the University of
         Calgary. My passion lies in creating software that will make a
-        meaningful impact on people's daily lives. Since graduating I have
+        meaningful impact on peoples day to day life. Since graduating I have
         helped local businesses develop static to full stack websites. I hope to
         expand my knowledge within this field and be able to solve complex
         problems through programming.
